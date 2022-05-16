56e654cab15b3f8160c5061b81b21a768e63633a
FZZStar <2631674847@qq.com>
2022-05-16 22:23:43 +0800

    80 @GetMapping和8001 @RequestBody Payment payment

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -3,10 +3,7 @@ package com.atguigu.springcloud.controller;
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
@@ -23,7 +20,7 @@ public class OrderController {
     @Resource
     private RestTemplate restTemplate;
 
-    @PostMapping("/consumer/payment/create")
+    @GetMapping("/consumer/payment/create")
     public CommonResult<Payment> create(Payment payment){
 
         log.info("******"+"\t"+"consumer调用了create");
