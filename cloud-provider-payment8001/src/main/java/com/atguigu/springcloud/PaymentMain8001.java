19bd5252eaaef6536347f55ee41ceb7d48bb8e4c
FZZStar <2631674847@qq.com>
2022-05-16 14:21:07 +0800

    cloud init - first commit

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -0,0 +1,15 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author fzzstart
+ * @create 2022-05-16 14:17
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
