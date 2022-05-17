98ac54a600f3ea16e1b5242aa5b84bce9e3bc730
FZZStar <2631674847@qq.com>
2022-05-17 16:27:46 +0800

    支付服务注册进zookeeper

---
+++ cloud-provider-payment8004/src/main/java/com/atguigu/springcloud/PaymentMain8004.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author fzzstart
+ * @create 2022-05-17 16:04
+ */
+@SpringBootApplication
+@EnableDiscoveryClient //服务发现，该注解用于向使用consul或者zookeeper作为注册中心时注册服务
+public class PaymentMain8004 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8004.class, args);
+    }
+}
---
+++ cloud-provider-payment8004/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,23 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @author fzzstart
+ * @create 2022-05-17 16:05
+ */
+@RestController
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping(value = "/payment/zk")
+    public String paymentzk() {
+        return "springcloud with zookeeper：" + serverPort + "\t" + UUID.randomUUID().toString();
+    }
+}
