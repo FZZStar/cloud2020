4f43a16d42b0a4493b6571e7274805b5aaaf6836
FZZStar <2631674847@qq.com>
2022-05-17 10:00:09 +0800

    80,8001注册进Eureka完成

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/MainApp80.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/MainApp80.java
@@ -3,12 +3,14 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author fzzstart
  * @create 2022-05-16 16:59
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class MainApp80 {
     public static void main(String[] args) {
         SpringApplication.run(MainApp80.class, args);
